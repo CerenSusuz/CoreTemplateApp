b639301661c05aae52065b62958c9ec34b1ac4c9
Ceren Susuz <ceren_susuz@epam.com>
2025-06-27 15:24:13 +0300

    feat(application): implement RegisterCommand with handler and validation

---
+++ CoreApp.Application/Features/Auth/Commands/Register/RegisterCommandValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreApp.Application.Features.Auth.Commands.Register
+{
+    public class RegisterCommandValidator : AbstractValidator<RegisterCommand>
+    {
+        public RegisterCommandValidator()
+        {
+            RuleFor(x => x.Request.Username)
+                .NotEmpty().WithMessage("Username is required.");
+
+            RuleFor(x => x.Request.Email)
+                .NotEmpty().EmailAddress();
+
+            RuleFor(x => x.Request.Password)
+                .NotEmpty().MinimumLength(6);
+        }
+    }
+}
