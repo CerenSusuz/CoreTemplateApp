c357625647cb0dda94056fd6b4663bc5f0d60db7
Ceren Susuz <ceren_susuz@epam.com>
2025-06-27 15:10:54 +0300

    feat(domain): add user, role and refresh token entities

---
+++ CoreApp.Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using CoreApp.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreApp.Domain.Entities
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public string PasswordHash { get; set; } = string.Empty;
+
+        public ICollection<Role> Roles { get; set; } = new List<Role>();
+
+        public ICollection<RefreshToken> RefreshTokens { get; set; } = new List<RefreshToken>();
+    }
+}
